690514efea860803475b853e78698829b480dde7
Vinícius França ♜ <viniciusicmsc@gmail.com>
2023-03-09 15:10:18 -0300

    Delete Javascript directory

--- Javascript/ifternario.js
+++
@@ -1,7 +0,0 @@
-parimpar(57)
-parimpar(28)
-
-
-function parimpar(num){
-    return num % 2 == 0 ? console.log(`Par`) : console.log(`Impar`) 
-}
--- Javascript/parametros2.js
+++
@@ -1,8 +0,0 @@
-function repeticoes(vezes) {
-    for (let i = 0; i < vezes; i++) {
-        array.push("+")
-    }
-    console.log(array)
-}
-let array = []
-repeticoes(4)
