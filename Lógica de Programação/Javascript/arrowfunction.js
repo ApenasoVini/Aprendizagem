44e99be1b0dc03aa333649630253766b15706a8f
Vinícius França ♜ <viniciusicmsc@gmail.com>
2023-03-14 17:15:24 -0300

    Add files via upload

---
+++ Lógica de Programação/Javascript/arrowfunction.js
@@ -0,0 +1,13 @@
+function calcularMedia(nota1, nota2, nota3, nota4) {
+    var media = (nota1 + nota2 + nota3 + nota4) / 4;
+    return media;
+  }
+  var nota1 = 7.5;
+  var nota2 = 8.0;
+  var nota3 = 7.5;
+  var nota4 = 9.5;
+
+  var media = calcularMedia(nota1, nota2, nota3, nota4);
+  
+  console.log(media);
+    
