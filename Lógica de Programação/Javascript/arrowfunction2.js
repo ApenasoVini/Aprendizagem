826678dc8aa62a3c5d78e33cf30b656a9b5f0cfc
Vinícius França ♜ <viniciusicmsc@gmail.com>
2023-03-15 15:16:11 -0300

    Add files via upload

---
+++ Lógica de Programação/Javascript/arrowfunction2.js
@@ -0,0 +1,21 @@
+soma = function soma(a, b){
+    return (a +b)
+
+}
+
+function soma2 (){
+    var num1 = 4, num2 = 5 
+    valor = num1 + num2
+    alert (valor)
+}
+
+
+multiplica = function multiplica(a, b){
+    return (a * b)
+}
+
+function multiplica2() {
+    var num1 = 3, num2 = 4 
+    valor = num1 * num2
+    alert (valor)
+}
