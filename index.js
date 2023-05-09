88871c730c8b7a0910e1b265d18bd3461df16b27
Vinícius França ♜ <viniciusicmsc@gmail.com>
2023-05-09 13:54:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
-        function versionamento() {
-            window.location.href = "https://github.com/ApenasoVini/Curso-Back-end/tree/main/Versionamento"
-        }
-        function Lógica() {
+function Lógica() {
             window.location.href = "https://github.com/ApenasoVini/Curso-Back-end/tree/main/L%C3%B3gica%20de%20Programa%C3%A7%C3%A3o"
         }
-        function Metodologias() {
-            window.location.href = "https://github.com/ApenasoVini/Curso-Back-end/tree/main/Metodologias%20%C3%81geis"
-        }
-      
-        function Banco_de_Dados() {
+function Banco_de_Dados() {
             window.location.href = "https://github.com/ApenasoVini/Curso/tree/main/Banco%20de%20Dados/SQL"
         }
-        
-                function APIs() {
+function APIs() {
             window.location.href = "https://github.com/ApenasoVini/Curso-Back-End/tree/main/APIs"
         }
