19a5b0ae991e8d657c2c86b0a511aac7f0035c2d
Vini <vi058587@intelbras.com.br>
2023-06-09 16:08:39 -0300

    Atualizações de lógica em JS

--- Lógica de Programação/Javascript/mesemnumero.js
+++ Lógica de Programação/Javascript/mesemnumero.js
@@ -1,41 +1,55 @@
 function mes(){
-    mesEmNumero = prompt(`Em que mês estamos?`)
+    
+    var mesEmNumero = prompt(`Em que mês estamos?`);
+
     switch(mesEmNumero){
+        
         case "1":
-            console.log(`Janeiro.`)
+            alert(`Janeiro.`);
             break;
-        case "2":
-            console.log(`Fevereiro.`)
+        
+            case "2":
+            alert(`Fevereiro.`);
             break;
-        case "3":
-            console.log(`Março.`)
+        
+            case "3":
+            alert(`Março.`);
             break;
-        case "4":
-            console.log(`Abril.`)
+        
+            case "4":
+            alert(`Abril.`);
             break;
-        case "5":
-            console.log(`Maio.`)
+        
+            case "5":
+            alert(`Maio.`);
             break;
-        case "6":
-            console.log(`Junho.`)
+        
+            case "6":
+            alert(`Junho.`);
             break;            
-        case "7":
-            console.log(`Julhlo.`)
+        
+            case "7":
+            alert(`Julhlo.`);
             break;
-        case "8":
-            console.log(`Agosto.`)
+        
+            case "8":
+            alert(`Agosto.`);
             break;
-        case "9":
-            console.log(`Setembro.`)
+        
+            case "9":
+            alert(`Setembro.`);
             break;
-        case "10":
-            console.log(`Outubro.`)
+        
+            case "10":
+            alert(`Outubro.`);
             break;
-        case "11":
-            console.log(`Novembro.`)
+        
+            case "11":
+            alert(`Novembro.`);
             break;
-        case "12":
-            console.log(`Dezembro.`)
+        
+            case "12":
+            alert(`Dezembro.`);
             break;
     }
 }
