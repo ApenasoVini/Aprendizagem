81a8e07363b471edd2812e3f1e1b9319acb44171
Vini <vi058587@intelbras.com.br>
2023-06-13 15:41:02 -0300

    Atualizações de JS

--- Lógica de Programação/Javascript/Calculadora/index.js
+++ Lógica de Programação/Javascript/Calculadora/index.js
@@ -1,17 +1,22 @@
 let display = document.getElementById('display');
 
-  function addToDisplay(value) {
-    display.value += value;
-  }
+function addToDisplay(value) {
+  display.value += value;
+}
 
-  function limpar(value) {
-    display.value = '';
+function limpar() {
+  display.value = '';
+}
+
+function limpar1() {
+  display.value = display.value.substring(0, display.value.length - 1);
+}
+
+function resultado() {
+  try {
+    let result = eval(display.value);
+    display.value = result;
+  } catch (e) {
+    console.log('Erro ao calcular: ' + e);
   }
-  function resultado() {
-    try {
-      let result = eval(display.value);
-      display.value = result;
-    } catch (e) {
-      console.log('Erro ao calcular: ' + e);
-    }
-  }
+}
