1e0c8c8a901cfa95f4c1a013667524d7ea454a61
Vini <vi058587@intelbras.com.br>
2023-06-15 14:36:51 -0300

    Atualizações de CSS

--- Site/index.js
+++
@@ -1,9 +0,0 @@
-function logica() {
-            window.location.href = "https://github.com/ApenasoVini/Curso-Back-end/tree/main/L%C3%B3gica%20de%20Programa%C3%A7%C3%A3o"
-        }
-function database() {
-            window.location.href = "https://github.com/ApenasoVini/Curso/tree/main/Banco%20de%20Dados/SQL"
-        }
-function apis() {
-            window.location.href = "https://github.com/ApenasoVini/Curso-Back-End/tree/main/APIs"
-        }
