767852dff38289c0a5ea261d1ae6e3f6261743e5
ApenasoVini <viniciusicmsc@gmail.com>
2023-06-26 16:24:36 -0300

    Nova API

--- APIs/GitFinder copy/index.js
+++ APIs/GitFinder copy/index.js
@@ -1,24 +1,20 @@
-let name;
-    
+let name, info;
+
 function poke() {
-        name = document.getElementById('name');
+    info = document.getElementById('info');
+    name = document.getElementById('name');
 
-
-        if(name){
+    if (name) {
         var url = `https://pokeapi.co/api/v2/pokemon/${name.value}`
         fetch(url)
-
             .then(res => res.json())
             .then(data => {
-
-                info.innerHTML =
-                    `
-            <p${name.value}>
-            
-            `
-
-            })}
-            else if(nome){
-                alert("Nome inválido!")
-            }
+                info.innerHTML = `
+                    <p>${data.name}</p>
+                `
+            })
     }
+    else if (!name) {
+        alert("Nome inválido!")
+    }
+}
