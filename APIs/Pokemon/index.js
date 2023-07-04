80395cef651d03416c898e43fa556dec0f365ac1
ApenasoVini <viniciusicmsc@gmail.com>
2023-07-04 09:18:37 -0300

    Atualizações

--- APIs/Pokemon/index.js
+++ APIs/Pokemon/index.js
@@ -1,31 +1,28 @@
 let nomePokemon;
 
 function dados() {
-    nomePokemon = document.getElementById('pokemonName');
+    nomePokemon = document.getElementById('pokemonName').value;
 }
 
 function poke() {
     dados();
 
     if (nomePokemon) {
-        var url = `https://pokeapi.co/api/v2/pokemon/${nomePokemon.value}`
+        var url = `https://pokeapi.co/api/v2/pokemon/${nomePokemon}`;
         fetch(url)
             .then(res => res.json())
             .then(data => {
-
                 document.getElementById('info').style.display = 'grid';
                 const infoPokemon = document.getElementById('info');
                 infoPokemon.innerHTML = `
-               
-                <span id='name'>${data.name}</span>
-                <span id='types'>${data.types[0].type.name}</span>
-                <span id='id'>${data.id}</span>
-                <img class='back_default'>${data.back_default}</span>
-                <img class='front_default'>${data.sprites.front_default}</span>
+                    <span id='name'>${data.name}</span>
+                    <span id='types'>${data.types[0].type.name}</span>
+                    <span id='id'>${data.id}</span>
+                    <img src='${data.sprites.back_default}' class='back_default'>
+                    <img src='${data.sprites.front_default}' class='front_default'>
                 `;
             });
-    }
-    else {
+    } else {
         alert('Nome inválido!');
     }
 }
