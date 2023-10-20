8ebc4e14144834ae6b3eeb9058fa5ec99852b74f
vi058587 <vinicius.franca@intelbras.com>
2023-10-20 15:45:37 -0300

    Aprendizagem

--- APIs/Carros/index.js
+++ APIs/Carros/index.js
@@ -1,22 +1,20 @@
-    let placa, modelo, marca, ano
-    function campos() {
-        placa = document.getElementById("placa");
-        if (placa.length == 8) {
-            return true
-        }
-        return false
+let placa, modelo, marca, ano
+function campos() {
+    placa = document.getElementById("placa");
+    if (placa.length == 8) {
+        return true
     }
-    function buscaCarro() {
-        campos()
-        var url = `https://my-json-server.typicode.com/ApenasoVini/Curso/Carros/db/placas/?placa=${placa.value.toUpperCase()}`
-        fetch(url)
-            .then(res => res.json())
-            .then(data => {
-
-                marca = data[0].marca
-                modelo = data[0].modelo
-                ano = data[0].ano
-
-                alert(`Placa: ${placa} \nMarca: ${marca} \nModelo: ${modelo} \nAno: ${ano}`)
-            })
-    }
+    return false
+}
+function buscaCarro() {
+    campos()
+    var url = `https://my-json-server.typicode.com/ApenasoVini/Curso/Carros/db/placas/?placa=${placa.value.toUpperCase()}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            marca = data[0].marca
+            modelo = data[0].modelo
+            ano = data[0].ano
+            alert(`Placa: ${placa} \nMarca: ${marca} \nModelo: ${modelo} \nAno: ${ano}`)
+        })
+}
--- APIs/GitFinder/index.js
+++ APIs/GitFinder/index.js
@@ -1,24 +1,20 @@
-    function buscaPerfil() {
-        event.preventDefault();
-        const nameUser = document.getElementById('usernameInput').value;
-        var followers, following, public_repos, avatar, login, linkRepos;
-        info = document.getElementById("info");
-
-        var url = `https://api.github.com/users/${nameUser}`
-        fetch(url)
-
-            .then(res => res.json())
-            .then(data => {
-
-                avatar = data.avatar_url;
-                login = data.login;
-                followers = data.followers;
-                following = data.following;
-                public_repos = data.public_repos;
-                linkRepos = data.repos_url;
-
-                info.innerHTML =
-                    `
+function buscaPerfil() {
+    event.preventDefault();
+    const nameUser = document.getElementById('usernameInput').value;
+    var followers, following, public_repos, avatar, login, linkRepos;
+    info = document.getElementById("info");
+    var url = `https://api.github.com/users/${nameUser}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            avatar = data.avatar_url;
+            login = data.login;
+            followers = data.followers;
+            following = data.following;
+            public_repos = data.public_repos;
+            linkRepos = data.repos_url;
+            info.innerHTML =
+                `
             <img src = "${avatar}">
             <p>Nome: ${login}<p>
             <p>Seguidores: ${followers}<p>
@@ -26,6 +22,5 @@
             <p>Repositórios: ${public_repos}<p>
             <p><a href="https://github.com/${nameUser}?tab=repositories" target="_blank">Ver Repositórios</a></p>
             `
-
-            })
-    }
+        })
+}
