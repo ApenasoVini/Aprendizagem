9db992f9531f3f52a4f4f8374e699646c1990840
vi058587 <vinicius.franca@intelbras.com>
2023-12-14 16:34:03 -0300

    js

--- Lógica de Programação/Javascript/arrowfunction.js
+++ Lógica de Programação/Javascript/arrowfunction.js
@@ -1,10 +1,3 @@
-const calcularMedia = () => {
-  var nota1 = 7.5;
-  var nota2 = 8.0;
-  var nota3 = 7.5;
-  var nota4 = 9.5;
-  var media = (nota1 + nota2 + nota3 + nota4) / 4;
-  return media;
-}
+const calcularMedia = (nota1, nota2, nota3, nota4) => (nota1 + nota2 + nota3 + nota4) / 4;
 
-calcularMedia();
+console.log(calcularMedia(4,5,4,4));
--- Lógica de Programação/Javascript/salario.js
+++
@@ -1,8 +0,0 @@
-function horasTrabalhadas(horasMes, horasPagamento){
-    horasMes = prompt(`Quantas horas por mês você trabalha?`)
-    horasPagamento = prompt(`Quanto você ganha por hora?`)
-    var salario = horasMes * horasPagamento 
-    console.log(`Seu salário é R$ ${salario}`)
-}
-
-horasTrabalhadas()
